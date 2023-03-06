a30715c03ca03e55e960df1328522254d4ecd178
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-06 14:28:27 -0600

    Changed file names and all references to files.

--- src/main/java/com/starbucksproject/starbucksposproject/PointOfSaleController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/PointOfSaleController.java
@@ -46,7 +46,7 @@ public class PointOfSaleController {
         stage.show();
     }
     public void switchToAlternatives(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("coffee-alternative.fxml"));
+        root = FXMLLoader.load(getClass().getResource("coffee-alternative-gui.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
