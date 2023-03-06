74ad8d280aed6ddea50f72810b374ec4d3fbd18c
Devon <devonkel4@tamu.edu>
2023-03-06 08:35:58 -0600

    Added Log In Screen without controller functionality

--- src/main/java/com/starbucksproject/starbucksposproject/PointOfSaleController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/PointOfSaleController.java
@@ -1,14 +1,97 @@
 package com.starbucksproject.starbucksposproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PointOfSaleController {
     @FXML
-    private Label welcomeText;
+    private TextField employeeID;
+    @FXML
+    private TextField employeePIN;
+    @FXML
+    private Button leftLogin;
+    @FXML
+    private Button button0;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void attemptLogin(){
+
+    }
+    @FXML
+    protected void setIdHighlight(){
+
+    }
+    @FXML
+    protected void setPinHighlight(){
+
+    }
+    @FXML
+    protected void clickButton0(){
+
+    }
+    @FXML
+    protected void clickButton1(){
+
+    }
+    @FXML
+    protected void clickButton2(){
+
+    }
+    @FXML
+    protected void clickButton3(){
+
+    }
+    @FXML
+    protected void clickButton4(){
+
+    }
+    @FXML
+    protected void clickButton5(){
+
+    }
+    @FXML
+    protected void clickButton6(){
+
+    }
+    @FXML
+    protected void clickButton7(){
+
+    }
+    @FXML
+    protected void clickButton8(){
+
+    }
+    @FXML
+    protected void clickButton9(){
+
+    }
+    @FXML
+    protected void clickButtonClear(){
+
+    }
+
+    @FXML
+    protected void clickButtonEnter(){
+
     }
 }
