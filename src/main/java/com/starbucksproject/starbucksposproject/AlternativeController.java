641450793e50d434633c2074189e2d50c8c7d942
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 07:42:31 -0600

    Added space for all functions on all buttons.

--- src/main/java/com/starbucksproject/starbucksposproject/AlternativeController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/AlternativeController.java
@@ -4,5 +4,87 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class AlternativeController {
+    @FXML
+    protected void clickTall() {
 
+    }
+    @FXML
+    protected void clickGrande() {
+
+    }
+    @FXML
+    protected void clickVenti() {
+
+    }
+    @FXML
+    protected void clickAddons() {
+
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickSteamedMilk() {
+
+    }
+    @FXML
+    protected void clickColdMilk() {
+
+    }
+    @FXML
+    protected void clickHotChocolate() {
+
+    }
+    @FXML
+    protected void clickHotWhiteChocolate() {
+
+    }
+    @FXML
+    protected void clickSoda() {
+
+    }
 }
