7a67f33247b24df19c11f763676d8ac07a6aab44
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 02:16:56 -0600

    Added space for all functions.

--- src/main/java/com/starbucksproject/starbucksposproject/BlendedController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/BlendedController.java
@@ -4,5 +4,119 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class BlendedController {
+    @FXML
+    protected void clickTall() {
 
+    }
+    @FXML
+    protected void clickGrande() {
+
+    }
+    @FXML
+    protected void clickVenti() {
+
+    }
+    @FXML
+    protected void clickAddons() {
+
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickCoffeeFrappuccino() {
+
+    }
+    @FXML
+    protected void  clickEspressoFrappuccino() {
+
+    }
+    @FXML
+    protected void clickMochaFrappuccino() {
+
+    }
+    @FXML
+    protected void clickWhiteMochaFrappuccino() {
+
+    }
+    @FXML
+    protected void clickCaramelFrappuccino() {
+
+    }
+    @FXML
+    protected void clickJavaChipFrappuccino() {
+
+    }
+    @FXML
+    protected void clickVanillaBeanCremeCrappuccino() {
+
+    }
+    @FXML
+    protected void clickStrawberryCremeFrappuccino() {
+
+    }
+    @FXML
+    protected void clickDoubleChocolateCremeFrappuccino() {
+
+    }
+    @FXML
+    protected void clickMatchaCremeFrappuccino() {
+
+    }
+    @FXML
+    protected void clickBlueberrySmoothie() {
+
+    }
+    @FXML
+    protected void clickPomegranateSmoothie() {
+
+    }
+    @FXML
+    protected void clickMangoSmoothie() {
+
+    }
 }
