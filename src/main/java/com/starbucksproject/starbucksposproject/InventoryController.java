16796f290f6af1504fb5ef1afc94141796d14040
jmaxm-tamu <jmaxm@tamu.edu>
2023-03-07 12:35:57 -0600

    Inventory GUI and Controller Added; Manager GUI tweaked

---
+++ src/main/java/com/starbucksproject/starbucksposproject/InventoryController.java
@@ -0,0 +1,35 @@
+package com.starbucksproject.starbucksposproject;
+
+import javafx.fxml.FXML;
+
+public class InventoryController {
+    @FXML
+    protected void clickServer() {
+
+    }
+    @FXML
+    protected void clickInventory() {
+
+    }
+    @FXML
+    protected void clickSales() {
+
+    }
+    @FXML
+    protected void clickTransactions() {
+
+    }
+    @FXML
+    protected void clickEmployees() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+
+    @FXML
+    protected void clickChangeInventory() {
+
+    }
+}
