55ea29a094fbf13e9bcfbbdeaade22f8c4b2981a
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 08:25:22 -0600

    Added manager-view.fxml and ManagerController.

---
+++ src/main/java/com/starbucksproject/starbucksposproject/ManagerController.java
@@ -0,0 +1,26 @@
+package com.starbucksproject.starbucksposproject;
+
+import javafx.fxml.FXML;
+
+public class ManagerController {
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickInventory() {
+
+    }
+    @FXML
+    protected void clickSales() {
+
+    }
+    @FXML
+    protected void clickTransactions() {
+
+    }
+    @FXML
+    protected void clickEmployees() {
+
+    }
+}
