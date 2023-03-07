ad97d63ab745511db608578b52a90f6d2b24d209
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 02:02:57 -0600

    Added space for all functions.

--- src/main/java/com/starbucksproject/starbucksposproject/EspressoController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/EspressoController.java
@@ -4,5 +4,160 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class EspressoController {
+    @FXML
+    protected void clickTall() {
+
+    }
+    @FXML
+    protected void clickGrande() {
+
+    }
+    @FXML
+    protected void clickVenti() {
+
+    }
+    @FXML
+    protected void clickAddons() {
+
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickAmericano() {
+
+    }
+    @FXML
+    protected void clickIcedAmericano() {
+
+    }
+    @FXML
+    protected void clickCappuccino() {
+
+    }
+    @FXML
+    protected void clickLatte() {
+
+    }
+    @FXML
+    protected void clickIcedLatte() {
+
+    }
+    @FXML
+    protected void clickCinnamonDolceLatte() {
+
+    }
+    @FXML
+    protected void clickIcedCinnamonDolceLatte() {
+
+    }
+    @FXML
+    protected void clickCaramelMacchiato() {
+
+    }
+    @FXML
+    protected void clickIcedCaramelMacchiato() {
+
+    }
+    @FXML
+    protected void clickCoconutMacchiato() {
+
+    }
+    @FXML
+    protected void clickIcedCoconutMacchiato() {
+
+    }
+    @FXML
+    protected void clickMocha() {
+
+    }
+    @FXML
+    protected void clickIcedMocha() {
+
+    }
+    @FXML
+    protected void clickWhiteMocha() {
+
+    }
+    @FXML
+    protected void clickIcedWhiteMocha() {
+
+    }
+    @FXML
+    protected void clickCoconutMocha() {
+
+    }
+    @FXML
+    protected void clickIcedCoconutMocha() {
+
+    }
+    @FXML
+    protected void clickBlackWhiteMocha() {
+
+    }
+    @FXML
+    protected void clickIcedBlackWhiteMocha() {
+
+    }
+    @FXML
+    protected void clickSolo() {
+
+    }
+    @FXML
+    protected void clickDouble() {
+
+    }
+    @FXML
+    protected void clickTriple() {
+
+    }
+    @FXML
+    protected void clickQuad() {
+
+    }
 
 }
