59339fcd42c2b471772c28d94a1cbec2302d44dd
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 07:20:22 -0600

    Added space for some functions.

--- src/main/java/com/starbucksproject/starbucksposproject/TeaController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/TeaController.java
@@ -3,4 +3,67 @@ package com.starbucksproject.starbucksposproject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 public class TeaController {
+    @FXML
+    protected void clickTall() {
+
+    }
+    @FXML
+    protected void clickGrande() {
+
+    }
+    @FXML
+    protected void clickVenti() {
+
+    }
+    @FXML
+    protected void clickAddons() {
+
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
 }
