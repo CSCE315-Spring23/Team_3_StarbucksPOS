c34a5874d33c97e97d31bc42b976b46538819b4c
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 01:42:05 -0600

    Added space for all functions.

--- src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
@@ -83,4 +83,12 @@ public class CoffeeController {
     protected void clickCafeLait() {
 
     }
+    @FXML
+    protected void clickColdBrew() {
+
+    }
+    @FXML
+    protected void clickCoffeeRefill() {
+
+    }
 }
