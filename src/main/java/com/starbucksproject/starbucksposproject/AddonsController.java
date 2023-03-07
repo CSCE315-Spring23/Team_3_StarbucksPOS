bc5b281fc483df4ea63bee1ee419a4d44e1ccf38
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 08:06:50 -0600

    Added space for all functions on all buttons.

--- src/main/java/com/starbucksproject/starbucksposproject/AddonsController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/AddonsController.java
@@ -4,5 +4,167 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class AddonsController {
+    @FXML
+    protected void clickAddons() {
 
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickEspressoShot() {
+
+    }
+    @FXML
+    protected void clickFrappuccinoRoast() {
+
+    }
+    @FXML
+    protected void clickClassicSyrup() {
+
+    }
+    @FXML
+    protected void clickVanillaSyrup() {
+
+    }
+    @FXML
+    protected void clickCaramelSyrup() {
+
+    }
+    @FXML
+    protected void clickCinnamonDolceSyrup() {
+
+    }
+    @FXML
+    protected void clickToffeeNutSyrup() {
+
+    }
+    @FXML
+    protected void clickMochaSauce() {
+
+    }
+    @FXML
+    protected void clickWhiteChocolateSauce() {
+
+    }
+    @FXML
+    protected void clickCinnamonDolceTopping() {
+
+    }
+    @FXML
+    protected void click2pMilk() {
+
+    }
+    @FXML
+    protected void clickWholeMilk() {
+
+    }
+    @FXML
+    protected void clickBreve() {
+
+    }
+    @FXML
+    protected void clickHeavyCream() {
+
+    }
+    @FXML
+    protected void clickSweetCream() {
+
+    }
+    @FXML
+    protected void clickWhippedCream() {
+
+    }
+    @FXML
+    protected void clickAlmondMilk() {
+
+    }
+    @FXML
+    protected void clickOatMilk() {
+
+    }
+    @FXML
+    protected void clickSoyMilk() {
+
+    }
+    @FXML
+    protected void clickCoconutMilk() {
+
+    }
+    @FXML
+    protected void clickLemonade() {
+
+    }
+    @FXML
+    protected void clickMatcha() {
+
+    }
+    @FXML
+    protected void clickStrawberryInclusions() {
+
+    }
+    @FXML
+    protected void clickDragonfruitInclusions() {
+
+    }
+    @FXML
+    protected void clickStrawberryAcaiBase() {
+
+    }
+    @FXML
+    protected void clickMangoDragonfruitBase() {
+
+    }
+    @FXML
+    protected void clickHoney() {
+
+    }
+    @FXML
+    protected void clickIce() {
+
+    }
 }
