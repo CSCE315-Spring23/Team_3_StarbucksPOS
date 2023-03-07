f512aa8d78d9444212a7114a975c666ff3f32349
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 01:40:50 -0600

    Added space for some functions.

--- src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
@@ -4,5 +4,83 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class CoffeeController {
+    @FXML
+    protected void clickTall() {
 
+    }
+    @FXML
+    protected void clickGrande() {
+
+    }
+    @FXML
+    protected void clickVenti() {
+
+    }
+    @FXML
+    protected void clickAddons() {
+
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickCoffee() {
+
+    }
+    @FXML
+    protected void clickCoffeeTraveller() {
+
+    }
+    @FXML
+    protected void clickIcedCoffee() {
+
+    }
+    @FXML
+    protected void clickCafeLait() {
+
+    }
 }
