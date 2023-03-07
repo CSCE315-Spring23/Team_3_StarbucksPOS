cdb8be96b1035ad17c5bd23e0b04ba740948b63b
YvanTonthat <yvan.tonthat@tamu.edu>
2023-03-07 07:55:50 -0600

    Added space for all functions on all buttons.

--- src/main/java/com/starbucksproject/starbucksposproject/FoodController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/FoodController.java
@@ -4,5 +4,135 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class FoodController {
+    @FXML
+    protected void clickAddons() {
 
+    }@FXML
+    protected void clickNext() {
+
+    }
+    @FXML
+    protected void clickManagerMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeMenu() {
+
+    }
+    @FXML
+    protected void clickEspressoMenu() {
+
+    }
+    @FXML
+    protected void clickBlendedMenu() {
+
+    }
+    @FXML
+    protected void clickTeasMenu() {
+
+    }
+    @FXML
+    protected void clickCoffeeAlternativesMenu() {
+
+    }
+    @FXML
+    protected void clickFoodMenu() {
+
+    }
+    @FXML
+    protected void clickBack() {
+
+    }
+    @FXML
+    protected void clickEditCustomerName() {
+
+    }
+    @FXML
+    protected void clickVoidLastItem() {
+
+    }
+    @FXML
+    protected void clickPay() {
+
+    }
+    @FXML
+    protected void clickBagel() {
+
+    }
+    @FXML
+    protected void clickMultigrainBagel() {
+
+    }
+    @FXML
+    protected void clickEverythingBagel() {
+
+    }
+    @FXML
+    protected void clickCreamCheese() {
+
+    }
+    @FXML
+    protected void clickCheeseDanish() {
+
+    }
+    @FXML
+    protected void clickAppleFritter() {
+
+    }
+    @FXML
+    protected void clickSaltedCaramelCookie() {
+
+    }
+    @FXML
+    protected void clickCinnamonRoll() {
+
+    }
+    @FXML
+    protected void clickBlueberryMuffin() {
+
+    }
+    @FXML
+    protected void clickTuxedoMuffin() {
+
+    }
+    @FXML
+    protected void clickBananaMuffin() {
+
+    }
+    @FXML
+    protected void clickSweetCroissant() {
+
+    }
+    @FXML
+    protected void clickToffeeBar() {
+
+    }
+    @FXML
+    protected void clickBrownie() {
+
+    }
+    @FXML
+    protected void clickChocolateChipCookie() {
+
+    }
+    @FXML
+    protected void clickSweetChocolateChipCookie() {
+
+    }
+    @FXML
+    protected void clickBakedChocolateChipCookie() {
+
+    }
+    @FXML
+    protected void clickZoesCookie() {
+
+    }
+    @FXML
+    protected void clickCheesecake() {
+
+    }
+    @FXML
+    protected void clickReesesBar() {
+
+    }
 }
