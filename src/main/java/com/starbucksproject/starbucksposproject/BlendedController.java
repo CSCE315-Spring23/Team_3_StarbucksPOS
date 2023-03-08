983b021e7518880627dbf177d66d0877eb12d322
jmaxm-tamu <jmaxm@tamu.edu>
2023-03-07 20:03:48 -0600

    Added functionality for main server menu switching buttons (Coffee menu, Espresso menu, etc)

--- src/main/java/com/starbucksproject/starbucksposproject/BlendedController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/BlendedController.java
@@ -2,8 +2,20 @@ package com.starbucksproject.starbucksposproject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
 
 public class BlendedController {
+    Connection conn = null;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     protected void clickTall() {
 
@@ -17,39 +29,74 @@ public class BlendedController {
 
     }
     @FXML
-    protected void clickAddons() {
-
-    }@FXML
+    public void clickAddons(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addons-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
     protected void clickNext() {
 
     }
     @FXML
-    protected void clickManagerMenu() {
+    public void clickManagerMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("manager-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void clickCoffeeMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("coffee-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void clickEspressoMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("espresso-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void clickBlendedMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("blended-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void clickTeasMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("tea-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
     @FXML
-    protected void clickCoffeeMenu() {
+    protected void clickCoffeeAlternativesMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("coffee-alternative-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
     @FXML
-    protected void clickEspressoMenu() {
-
-    }
-    @FXML
-    protected void clickBlendedMenu() {
-
-    }
-    @FXML
-    protected void clickTeasMenu() {
-
-    }
-    @FXML
-    protected void clickCoffeeAlternativesMenu() {
-
-    }
-    @FXML
-    protected void clickFoodMenu() {
-
+    protected void clickFoodMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("food-gui.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     @FXML
     protected void clickBack() {
