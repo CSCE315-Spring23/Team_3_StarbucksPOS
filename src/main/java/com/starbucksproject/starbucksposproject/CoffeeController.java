7cb52770ce3dc3fb323b18cf6dc405ff7f2b4468
David Liu <david_liu@tamu.edu>
2023-03-07 21:29:54 -0600

    clickCoffee() changed, weeded out sout. Merge uncommited changes into BaseUI.

--- src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/CoffeeController.java
@@ -2,91 +2,179 @@ package com.starbucksproject.starbucksposproject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.HexFormat;
+import javafx.scene.input.KeyEvent;
 
 public class CoffeeController {
+
+    Connection conn = null;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    int currentSize = 0;
+
     @FXML
     protected void clickTall() {
-
+        currentSize = 0;
     }
+
     @FXML
     protected void clickGrande() {
-
+        currentSize = 1;
     }
+
     @FXML
     protected void clickVenti() {
-
+        currentSize = 2;
     }
-    @FXML
-    protected void clickAddons() {
 
-    }@FXML
+    @FXML
+    public void clickAddons(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addons-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
     protected void clickNext() {
 
     }
+
     @FXML
-    protected void clickManagerMenu() {
+    public void clickManagerMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("manager-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void clickCoffeeMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("coffee-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void clickEspressoMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("espresso-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void clickBlendedMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("blended-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void clickTeasMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("tea-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
+
     @FXML
-    protected void clickCoffeeMenu() {
+    protected void clickCoffeeAlternativesMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("coffee-alternative-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
     }
-    @FXML
-    protected void clickEspressoMenu() {
 
-    }
     @FXML
-    protected void clickBlendedMenu() {
-
+    protected void clickFoodMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("food-gui.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-    @FXML
-    protected void clickTeasMenu() {
 
-    }
-    @FXML
-    protected void clickCoffeeAlternativesMenu() {
-
-    }
-    @FXML
-    protected void clickFoodMenu() {
-
-    }
     @FXML
     protected void clickBack() {
 
     }
+
     @FXML
     protected void clickEditCustomerName() {
 
     }
+
     @FXML
     protected void clickVoidLastItem() {
 
     }
+
     @FXML
     protected void clickPay() {
 
     }
+
     @FXML
     protected void clickCoffee() {
-
+        if (currentSize == 0) {
+            CurrentOrderList.getInstance().getCurrentOrder().add("101001");
+        } else if (currentSize == 1) {
+            CurrentOrderList.getInstance().getCurrentOrder().add("101002");
+        } else {
+            CurrentOrderList.getInstance().getCurrentOrder().add("101003");
+        }
+        System.out.println(CurrentOrderList.getInstance().getCurrentOrder());
     }
+
     @FXML
     protected void clickCoffeeTraveller() {
 
     }
+
     @FXML
     protected void clickIcedCoffee() {
 
     }
+
     @FXML
     protected void clickCafeLait() {
 
     }
+
     @FXML
     protected void clickColdBrew() {
 
     }
+
     @FXML
     protected void clickCoffeeRefill() {
 
