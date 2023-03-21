df84a6ebedd924829002d26f34114885e3e41464
jmaxm-tamu <jmaxm@tamu.edu>
2023-03-21 18:26:18 -0500

    Removed "Excess Report" Button, changed Sales Report to Sales Between Dates (due to misunderstanding)

--- src/main/java/com/starbucksproject/starbucksposproject/SalesController.java
+++ src/main/java/com/starbucksproject/starbucksposproject/SalesController.java
@@ -107,11 +107,96 @@ public class SalesController implements Initializable {
         stage.show();
     }
     @FXML
-    protected void clickSalesReport(ActionEvent event) throws IOException {
-        // Enter SalesReport actions here
+    protected void clickSalesBounded(ActionEvent event) throws IOException {
         // Enter SalesReport actions here:
-        int startDate = Integer.parseInt(fromDate.getText());
-        int endDate = Integer.parseInt(toDate.getText());
+        conn = DBConnection.getInstance().getConnection();
+
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Sales Report");
+        dialog.setHeaderText("Select start date and end date:");
+        ButtonType selectButtonType = new ButtonType("Enter", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(selectButtonType, ButtonType.CANCEL);
+        ChoiceBox<String> inventoryChoiceBox = new ChoiceBox<>();
+        TextField fromDate = new TextField();
+        TextField toDate = new TextField();
+        fromDate.setText("000000");
+        toDate.setText("000000");
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+        grid.add(new Label("Start date: (YYMMDD)"), 0, 0);
+        grid.add(fromDate, 1, 0);
+        grid.add(new Label("End date: (YYMMDD)"), 0, 1);
+        grid.add(toDate, 1, 1);
+        Node selectButton = dialog.getDialogPane().lookupButton(selectButtonType);
+        selectButton.setDisable(false);
+        dialog.getDialogPane().setContent(grid);
+
+        Platform.runLater(() -> fromDate.requestFocus());
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == selectButtonType) {
+                int startDate = Integer.parseInt(fromDate.getText());
+                int endDate = Integer.parseInt(toDate.getText());
+
+                return new Pair<>(String.format("%d", startDate), String.format("%d", endDate));
+            }
+            return null;
+        });
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+        result.ifPresent(pair -> {
+
+
+            int startDate = 0;
+            int endDate = 0;
+
+            startDate = Integer.parseInt(fromDate.getText());
+            endDate = Integer.parseInt(toDate.getText());
+
+            try {
+                final String query = "SELECT * FROM sales ORDER BY date DESC";
+                PreparedStatement tableQuery = conn.prepareStatement(query);
+                ResultSet response = tableQuery.executeQuery();
+                ObservableList<SalesItem> items = FXCollections.observableArrayList();
+
+                ObservableList<TableColumn> columns = salesTable.getColumns();
+                columns.get(0).setCellValueFactory(new PropertyValueFactory<>("day"));
+                columns.get(1).setCellValueFactory(new PropertyValueFactory<>("date"));
+                columns.get(2).setCellValueFactory(new PropertyValueFactory<>("week"));
+                columns.get(3).setCellValueFactory(new PropertyValueFactory<>("year"));
+                columns.get(4).setCellValueFactory(new PropertyValueFactory<>("game_day"));
+                columns.get(5).setCellValueFactory(new PropertyValueFactory<>("sales"));
+
+                while (response.next()) {
+                    int day = response.getInt("day");
+                    int date = response.getInt("date");
+                    int week = response.getInt("week");
+                    int year = response.getInt("year");
+                    boolean game_day = response.getBoolean("game_day");
+                    double sales = response.getDouble("sales");
+
+                    if (date >= startDate && date <= endDate) {
+                        SalesItem item = new SalesItem(day, date, week, year, game_day, sales);
+                        items.add(item);
+                    }
+                }
+
+                salesTable.setItems(items);
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//            System.exit(0);
+            }
+
+        });
+    }
+
+    @FXML
+    protected void clickReset(ActionEvent event) throws IOException {
         conn = DBConnection.getInstance().getConnection();
         try {
             final String query = "SELECT * FROM sales ORDER BY date DESC";
@@ -135,10 +220,8 @@ public class SalesController implements Initializable {
                 boolean game_day = response.getBoolean("game_day");
                 double sales = response.getDouble("sales");
 
-                if(date >= startDate && date <= endDate) {
-                    SalesItem item = new SalesItem(day, date, week, year, game_day, sales);
-                    items.add(item);
-                }
+                SalesItem item = new SalesItem(day, date, week, year, game_day, sales);
+                items.add(item);
             }
 
             salesTable.setItems(items);
@@ -150,21 +233,6 @@ public class SalesController implements Initializable {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
 //            System.exit(0);
         }
-
-    }
-
-    protected void clickSalesFromTo(ActionEvent event) throws IOException{
-
-    }
-
-    @FXML
-    protected void clickExcessReport(ActionEvent event) throws IOException {
-
-    }
-
-    @FXML
-    protected void clickReset(ActionEvent event) throws IOException {
-
     }
     /**
      * Called to initialize a controller after its root element has been
